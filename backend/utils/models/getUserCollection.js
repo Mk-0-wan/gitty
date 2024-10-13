b7174ad6a3425b37051bb341147037ffaf707646
gh057 <victormurithi2020@gmail.com>
2024-10-13 23:52:37 +0300

    Improved the crud handler methods
    - Used try catch for easy error handling
     - Added some new handlers that will be used to perform crud operation.

--- backend/utils/models/getUserCollection.js
+++ backend/utils/models/getUserCollection.js
@@ -6,16 +6,16 @@ const postNewUser = async (data, db, collectionName) => {
   try {
     const result = await db.collection(collectionName).insertOne(data)
     return result
-  } catch(err) {
+  } catch (err) {
     throw new Error(`Failed to upload the new user to the database ${err.message}`);
   }
 }
 
 const updateUser = async (query, data, db, collectionName) => {
   try {
-    const updatedUser = await db.collection(collectionName).updateOne(query, { $set: data});
+    const updatedUser = await db.collection(collectionName).updateOne(query, { $set: data });
     return updatedUser;
-  } catch(err) {
+  } catch (err) {
     throw new Error(`Failed to upload the new user to the database ${err.message}`);
   }
 }
@@ -25,7 +25,7 @@ const deleteUser = async (query, db, collectionName) => {
     const deleted = await db.collection(collectionName).deleteOne(query);
     if (deleted.machtedCount === 0) throw new Error('No such user in the database');
     return { success: true, deleted };
-  } catch(err) {
+  } catch (err) {
     throw new Error(`Failed to delete the user ${err.message}`);
   }
 }
@@ -34,7 +34,7 @@ const findUser = async (query = {}, db, collectionName) => {
   try {
     const user = await db.collection(collectionName).find(query).toArray();
     return user
-  } catch(err) {
+  } catch (err) {
     throw new Error(`Failed to load user from database ${err.message}`);
   }
 }
@@ -43,7 +43,7 @@ const countUsers = async (db, collectionName) => {
   try {
     const count = await db.collection(collectionName).countDocuments({}, { hint: "_id_" });
     return count;
-  } catch(err) {
+  } catch (err) {
     throw new Error(`Failed to query users detail ${err.message}`);
   }
 }
