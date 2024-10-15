3f2154094cd43ca452261f00ac7cb1fae45582b0
gh057 <victormurithi2020@gmail.com>
2024-10-15 03:30:13 +0300

    Feat(pluggins): Added two new pluggins

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-import Fastify from "fastify"
+import Fastify from "fastify";
 import dotenv from "dotenv";
 import routes from "./routes/firstRoute.js";
-import dbConnector from "./plugins/mongodb.js"
+import dbConnector from "./plugins/mongodb.js";
+import secureSession from "./plugins/session.js";
+import passPort from "./plugins/passport.js";
 
 dotenv.config();
 
@@ -14,13 +16,15 @@ const createApp = () => {
     },
   });
   // register all the routes
-  fastify.register(dbConnector)
+  fastify.register(dbConnector);
   fastify.addHook(
     "preHandler",
     async (request, _) => {
       request.db = fastify.mongo.db;
     })
-  fastify.register(routes)
+  fastify.register(secureSession);
+  fastify.register(passPort);
+  fastify.register(routes);
 
   fastify.listen({ port: process.env.PORT }, ((err) => {
     if (err) {
