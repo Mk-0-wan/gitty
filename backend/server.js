af82a3a23139d0b07b93fde7e7bcb1fbb5303d20
gh057 <victormurithi2020@gmail.com>
2024-10-22 12:31:53 +0300

    Feat(backend): Added some new hooks redis

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import routes from "./routes/firstRoute.js";
 import dbConnector from "./plugins/mongodb.js";
 import secureSession from "./plugins/session.js";
 import passPort from "./plugins/passport.js";
+import cors from "./plugins/cors.js";
+import redis from "./plugins/redis.js";
 
 dotenv.config();
 
@@ -15,13 +17,22 @@ const createApp = () => {
       },
     },
   });
-  // register all the routes
+  // avoid cross site origin error 
+  fastify.register(cors);
+  fastify.register(redis);
+  // register all the routes pluggins that you have created here
   fastify.register(dbConnector);
   fastify.addHook(
     "preHandler",
     async (request, _) => {
       request.db = fastify.mongo.db;
     })
+  fastify.addHook(
+    "preHandler",
+    async (req, _) => {
+      req.redis = fastify.redis;
+    }
+  )
   fastify.register(secureSession);
   fastify.register(passPort);
   fastify.register(routes);
