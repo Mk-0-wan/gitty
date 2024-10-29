03b22ab308bf990bd5a90a6fa40f466e743f7b24
gh057 <victormurithi2020@gmail.com>
2024-10-29 13:00:23 +0300

    Fix(backend): Made some changes to the files

--- backend/plugins/cors.js
+++ backend/plugins/cors.js
@@ -4,12 +4,10 @@ import fastifyPlugin from "fastify-plugin";
 
 async function cors(fastify) {
   fastify.register(fastifyCors, {
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['set-cookie'],
+    origin: ['http://localhost:5173'], // Your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow the necessary methods
+    credentials: true, // This allows cookies to be sent along with requests
+    allowedHeaders: ['Content-Type', 'Authorization'], // Important for JWT and other headers
   });
 }
-
 export default fastifyPlugin(cors);
