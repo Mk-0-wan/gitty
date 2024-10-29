ffcdccd4b3dade66aeaa1d2519a3c9dc76ac0eab
gh057 <victormurithi2020@gmail.com>
2024-10-29 13:13:01 +0300

    Feat(frontend): Simple dash layout

--- frontend/src/Layouts/Dashboard/LogOut.jsx
+++ frontend/src/Layouts/Dashboard/LogOut.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../Context/AuthProvider";
 
 export default function LogOut() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    localStorage.removeItem("token");
-    navigate("/", { state: { from: location } })
-  }, [navigate, location]);
-  return;
+  const { logout } = useAuth();
+  return (
+    <>
+      <NavLink
+        to="/"
+        onClick={logout}
+        className="hover:border-blue-500 transition duration-2 w-72 m-3 pt-2.5 pb-2.5 border border-blue-950 text-current flex gap-4 justify-center rounded-full"
+      >
+        Click Me
+      </NavLink>
+    </>
+  );
 }
-
